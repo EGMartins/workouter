60aceda787d30e1fe48c5f381c0e37628f58417e
Eduardo Gomes Martins <mirassol.code@gmail.com>
2017-07-17 01:40:55 -0300

    Adding some style with Bootstrap

--- spec/features/creating_home_page_spec.rb
+++ spec/features/creating_home_page_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "Creating Home Page" do
   scenario do
     visit '/'
 
-    expect(page).to have_link('workouter')
+    expect(page).to have_link('Workouter')
     expect(page).to have_link('Home')
     expect(page).to have_content('Workout Lounge') 
     expect(page).to have_content('Show off your workout')
